f46898fd3c53e9ccfd3a6ce7b33c3855ce5922c7
Strahil731 <hristovstrahil5@gmail.com>
2025-07-28 10:56:26 +0300

    Create Числата от  100 до 200.js

---
+++ Числата от  100 до 200.js
@@ -0,0 +1,13 @@
+function numberBetween100and200(number) {
+    if (number < 100) {
+        console.log("Less than 100");
+    }
+    else if (number >= 100 && number <= 200) {
+        console.log("Between 100 and 200");
+    }
+    else if (number > 200) {
+        console.log("Greater than 200");
+    }
+}
+
+numberBetween100and200(95)
