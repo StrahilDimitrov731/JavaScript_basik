f0793f73d05d58dc1b8883d327dab7ad6a17d6ef
Strahil731 <hristovstrahil5@gmail.com>
2025-08-01 09:06:07 +0300

    Create Ден от седмицата.js

---
+++ Ден от седмицата.js
@@ -0,0 +1,28 @@
+function dayOfWeek(number) {
+    if (number == 1) {
+        console.log("Понеделник");
+    }
+    else if (number == 2) {
+        console.log("Вторник");
+    }
+    else if (number == 3) {
+        console.log("Сряда");
+    }
+    else if (number == 4) {
+        console.log("Четвъртък");
+    }
+    else if (number == 5) {
+        console.log("Петък");
+    }
+    else if (number == 6) {
+        console.log("Събота");
+    }
+    else if (number == 7) {
+        console.log("Неделя");
+    }
+    else {
+        console.log("ГРЕШКА!");
+    }
+}
+
+dayOfWeek(5)
