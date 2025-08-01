b769544b5057cd4a6bf525f25d9d94223acda26e
Strahil731 <hristovstrahil5@gmail.com>
2025-08-01 09:51:48 +0300

    Create Квартално магазинче.js

---
+++ Квартално магазинче.js
@@ -0,0 +1,73 @@
+function superMarker(input) {
+    let product = input.shift();
+    let town = input.shift();
+    let quantity = Number(input.shift());
+
+    let price = 0;
+
+    switch (town) {
+        case "Sofia":
+            if (product == "coffee") {
+                price = quantity * 0.50;
+            }
+            else if (product == "water") {
+                price = quantity * 0.80;
+            }
+            else if (product == "beer") {
+                price = quantity * 1.20;
+            }
+            else if (product == "sweets") {
+                price = quantity * 1.45;
+            }
+            else if (product == "peanuts") {
+                price = quantity * 1.60;
+            }
+            break;
+
+        case "Plovdiv":
+            if (product == "coffee") {
+                price = quantity * 0.40;
+            }
+            else if (product == "water") {
+                price = quantity * 0.70;
+            }
+            else if (product == "beer") {
+                price = quantity * 1.15;
+            }
+            else if (product == "sweets") {
+                price = quantity * 1.30;
+            }
+            else if (product == "peanuts") {
+                price = quantity * 1.50;
+            }
+            break;
+
+        case "Varna":
+            if (product == "coffee") {
+                price = quantity * 0.45;
+            }
+            else if (product == "water") {
+                price = quantity * 0.70;
+            }
+            else if (product == "beer") {
+                price = quantity * 1.10;
+            }
+            else if (product == "sweets") {
+                price = quantity * 1.35;
+            }
+            else if (product == "peanuts") {
+                price = quantity * 1.55;
+            }
+            break;
+    }
+
+    console.log(price);
+}
+
+superMarker(
+    [
+        "beer",
+        "Sofia",
+        "2"
+    ]
+)
