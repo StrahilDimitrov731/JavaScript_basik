7a70975d52832386467f71f125b12953b35b8eb9
Strahil731 <hristovstrahil5@gmail.com>
2025-08-01 09:16:51 +0300

    Create Клас животно.js

---
+++ Клас животно.js
@@ -0,0 +1,17 @@
+function classAnimal(animal) {
+    switch (animal) {
+        case "dog":
+            console.log("mammal");
+            break;
+        case "crocodile":
+        case "tortoise":
+        case "snake":
+            console.log("reptile");
+            break;
+        default:
+            console.log("unknown");
+            break;
+    }
+}
+
+classAnimal("cat")
