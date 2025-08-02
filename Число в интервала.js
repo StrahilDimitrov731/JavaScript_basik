4f23e8abbd1419b286f4d02c3feb44280fff102a
Strahil731 <hristovstrahil5@gmail.com>
2025-08-02 10:09:16 +0300

    Create Число в интервала.js

---
+++ Число в интервала.js
@@ -0,0 +1,12 @@
+function numberInInterval(number) {
+    if (number >= -100 && number <= 100) {
+        if (number !== 0) {
+            console.log("YES");
+        }
+        else {
+            console.log("NO");
+        }
+    }
+}
+
+numberInInterval(25)
